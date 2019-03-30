caf98bc1c61e2e712c84e2f84fe428f89cd82126
Andrew James <ajames20@gmail.com>
2019-03-30 11:03:32 -0500

    Add ability to create place

--- App.js
+++ App.js
@@ -1,32 +1,52 @@
 import React from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { StyleSheet, TextInput, Text, View, Button } from "react-native";
 
 export default class App extends React.Component {
   state = {
-    placeName: ""
+    placeName: "",
+    places: []
   };
 
-  placeNameUpdater = event => {
+  placeNameUpdater = value => {
     this.setState({
-      placeName: event.target.value
+      placeName: value
     });
   };
 
-  render() {
+  placeSubmitHandler = () => {
     const { placeName } = this.state;
+    if (placeName.trim() === "") {
+      return;
+    }
+
+    this.setState(prevState => ({
+      places: prevState.places.concat(prevState.placeName)
+    }));
+  };
+
+  render() {
+    const { placeName, places } = this.state;
 
     return (
       <View style={styles.container}>
-        <TextInput
-          name="placeName"
-          value={placeName}
-          onChange={this.placeNameUpdater}
-          style={{
-            width: 300,
-            borderColor: "black",
-            borderWidth: 1
-          }}
-        />
+        <View style={styles.inputContainer}>
+          <TextInput
+            name="placeName"
+            value={placeName}
+            onChangeText={this.placeNameUpdater}
+            style={styles.placeInput}
+          />
+          <Button
+            style={styles.placeButton}
+            onPress={this.placeSubmitHandler}
+            title="Add"
+          />
+        </View>
+        <View>
+          {places.map((place, index) => (
+            <Text key={index}>{place}</Text>
+          ))}
+        </View>
       </View>
     );
   }
@@ -34,9 +54,24 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    padding: 100,
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "flex-start"
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%"
+  },
+  placeInput: {
+    width: "70%",
+    borderColor: "black",
+    borderWidth: 1
+  },
+  placeButton: {
+    width: "30%"
   }
 });
