aea073b343236f11fc46354ed64168ca664ca8e1
Andrew James <ajames20@gmail.com>
2019-03-30 10:29:17 -0500

    Add eslint-prettier config format App

--- App.js
+++ App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
 
 export default class App extends React.Component {
   state = {
-    placeName: ''
-  }
+    placeName: ""
+  };
 
   placeNameUpdater = event => {
     this.setState({
       placeName: event.target.value
     });
-  }
+  };
 
   render() {
     const { placeName } = this.state;
 
     return (
       <View style={styles.container}>
-        <TextInput name="placeName" value={placeName} onChange={this.placeNameUpdater} style={{
-          width: 300, borderColor: 'black',
-          borderWidth: 1
-        }}/>
+        <TextInput
+          name="placeName"
+          value={placeName}
+          onChange={this.placeNameUpdater}
+          style={{
+            width: 300,
+            borderColor: "black",
+            borderWidth: 1
+          }}
+        />
       </View>
     );
   }
@@ -29,8 +35,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
