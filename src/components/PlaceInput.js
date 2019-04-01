c28fe924ba72489dc0201249987c818170d39438
Andrew James <ajames20@gmail.com>
2019-03-31 19:14:41 -0500

    Let place input manage input state

--- src/components/PlaceInput.js
+++ src/components/PlaceInput.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, View, TextInput, Button } from "react-native";
 
-const PlaceInput = props => (
-  <View style={styles.inputContainer}>
-    <TextInput
-      name="placeName"
-      value={props.placeName}
-      onChangeText={props.placeNameUpdater}
-      style={styles.placeInput}
-    />
-    <Button
-      style={styles.placeButton}
-      onPress={props.placeSubmitHandler}
-      title="Add"
-    />
-  </View>
-);
+class PlaceInput extends Component {
+  state = {
+    placeName: ""
+  };
+
+  placeNameUpdater = value => {
+    this.setState({
+      placeName: value
+    });
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.props.onPlaceAdded(this.state.placeName);
+    this.setState({ placeName: "" });
+  };
+
+  render() {
+    return (
+      <View style={styles.inputContainer}>
+        <TextInput
+          name="placeName"
+          value={this.state.placeName}
+          onChangeText={this.placeNameUpdater}
+          style={styles.placeInput}
+        />
+        <Button
+          style={styles.placeButton}
+          onPress={this.placeSubmitHandler}
+          title="Add"
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   inputContainer: {
