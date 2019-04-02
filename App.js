069b483482c24f9cf297a8275e0e1bb6a6c72185
Andrew James <ajames20@gmail.com>
2019-04-02 06:47:01 -0500

    Add selected place to app and modal

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import PlaceInput from "./src/components/PlaceInput";
 import PlaceList from "./src/components/PlaceList";
+import PlaceDetail from "./src/components/PlaceDetail";
 
 import placeImage from "./src/assets/beach.jpg";
 
 export default class App extends React.Component {
   state = {
-    places: []
+    places: [],
+    selectedPlace: null
   };
 
   onPlaceAdded = placeName => {
@@ -15,24 +17,30 @@ export default class App extends React.Component {
       places: prevState.places.concat({
         key: Math.random(),
         name: placeName.trim(),
-        image: placeImage
+        image: {
+          uri: "https://bit.ly/2FZA8gE"
+        }
       })
     }));
   };
 
-  deletePlace = key => {
+  placeSelectedHandler = key => {
     this.setState(prevState => ({
-      places: prevState.places.filter(place => place.key !== key)
+      selectedPlace: prevState.places.find(place => place.key === key)
     }));
+    // this.setState(prevState => ({
+    //   places: prevState.places.filter(place => place.key !== key)
+    // }));
   };
 
   render() {
-    const { places } = this.state;
+    const { places, selectedPlace } = this.state;
 
     return (
       <View style={styles.container}>
+        <PlaceDetail selectedPlace={selectedPlace} />
         <PlaceInput onPlaceAdded={this.onPlaceAdded} />
-        <PlaceList places={places} deletePlace={this.deletePlace} />
+        <PlaceList places={places} onItemSelected={this.placeSelectedHandler} />
       </View>
     );
   }
