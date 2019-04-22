ac3c8145f88b4542ef482f57f3ed9a95e276034c
Andrew James <ajames20@gmail.com>
2019-04-21 20:24:49 -0500

    Add redux dev tools set up

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { AppRegistry } from "react-native";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 
 import rootReducer from "./src/store/configureStore";
 import Main from "./src/components/Main";
 
+let composeEnhancers = compose;
+
+if (__DEV__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 class App extends React.Component {
-  store = createStore(rootReducer);
+  store = createStore(rootReducer, composeEnhancers());
 
   render() {
     return (
