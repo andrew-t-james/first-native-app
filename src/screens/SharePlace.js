b2c8498351aee03dbe9a081fa65cef138cdbd102
Andrew James <ajames20@gmail.com>
2019-04-23 06:36:17 -0500

    Create SharePlace screen

---
+++ src/screens/SharePlace.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class SharePlace extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Share Place!</Text>
+      </View>
+    );
+  }
+}
+
+export default SharePlace;
