432ba7647fa5ef14e12b020472597ad4a29b8b56
Andrew James <ajames20@gmail.com>
2019-04-22 20:36:20 -0500

    Add tab and auth

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,10 +1,59 @@
-import { createStackNavigator, createAppContainer } from "react-navigation";
+/* eslint-disable react/display-name */
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Text, View, Button, StyleSheet, AsyncStorage } from "react-native";
+import {
+  createSwitchNavigator,
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator
+} from "react-navigation";
+
+import { Ionicons } from "@expo/vector-icons";
+
 import Main from "../components/Main";
+import SignInScreen from "./SignInScreen";
+import AuthLoadingScreen from "./AuthLoadingScreen";
+import SettingsScreen from "./SettingsScreen";
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: Main
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: Main,
+    Settings: SettingsScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === "Home") {
+          iconName = `ios-information-circle${focused ? "" : "-outline"}`;
+        } else if (routeName === "Settings") {
+          iconName = `ios-options`;
+        }
+        return <Ionicons size={20} name={iconName} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray"
+    }
   }
-});
+);
 
-export default createAppContainer(AppNavigator);
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      AuthLoading: AuthLoadingScreen,
+      App: TabNavigator,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "AuthLoading"
+    }
+  )
+);
