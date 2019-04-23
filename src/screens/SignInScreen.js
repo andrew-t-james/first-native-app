aa2e2aa39dde0b3b0186503531cdd28e923d825f
Andrew James <ajames20@gmail.com>
2019-04-22 20:36:11 -0500

    Create SiginInScreen

---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Button, AsyncStorage } from "react-native";
+
+class SignInScreen extends React.Component {
+  static navigationOptions = {
+    title: "Please sign in"
+  };
+
+  _signInAsync = async () => {
+    await AsyncStorage.setItem("userToken", "abc");
+    this.props.navigation.navigate("App");
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Button title="Sign in!" onPress={this._signInAsync} />
+      </View>
+    );
+  }
+}
+
+export default SignInScreen;
