73f605ff841c6eba35e99604ca069a48223247a0
Andrew James <ajames20@gmail.com>
2019-04-24 19:20:17 -0500

    Add new set up for navigation

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -8,7 +8,7 @@ class SignInScreen extends React.Component {
 
   _signInAsync = async () => {
     await AsyncStorage.setItem("userToken", "abc");
-    this.props.navigation.navigate("App");
+    this.props.navigation.navigate("Dashboard");
   };
 
   render() {
